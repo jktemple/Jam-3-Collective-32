6497be0cbf8eb59fbe83481da8dae56506621546
berrynul <nfonken@ucsc.edu>
2022-11-29 19:28:57 -0800

    changes to the organization of word processing
    -The word class is put into a seperate script
    -Everything should remain the same, this is mostly for organizational purposes in case the word class is used outside word manager.

---
+++ Jam 3 Collective 32/Assets/Scenes/Scripts/WordStruc.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Words
+{
+    public string word;
+    public int ID;
+    public float[] Notes;
+    
+
+    //find an Attribute that lets you select the element from 3 choices
+    public string element;
+    public string stance;
+    public string next;
+
+}  
+
